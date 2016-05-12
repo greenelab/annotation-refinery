adfad3b2850c086af3fbabb3936db44b85265e37
Rene A. Zelaya <4641983+rzelayafavila@users.noreply.github.com>
2016-05-12 17:18:13 -0400

    Adding functionality to create new geneset versions if a geneset with the same 'slug', and creator username exists. However, this mechanism will not create a version if the annotations have not changed between the version that is saved in Tribe and the one that has just been processed by the refinery.

--- loader.py
+++ loader.py
@@ -1,6 +1,9 @@
 import sys
 import json
+import requests
 from ConfigParser import SafeConfigParser
+
+from utils import translate_gene_ids
 
 # Import and set logger
 import logging
@@ -8,10 +11,10 @@ logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
 
-def load_to_tribe(main_config_file, geneset_info):
+def load_to_tribe(main_config_file, geneset_info, create_new_versions=False):
     try:
         from tribe_client.utils import obtain_token_using_credentials, \
-            create_remote_geneset
+            create_remote_geneset, create_remote_version
     except ImportError:
         logger.error('The package "tribe-client" has not been installed in '
                      'this Python environment. Please pip-install it to'
@@ -56,10 +59,73 @@ def load_to_tribe(main_config_file, geneset_info):
     access_token = obtain_token_using_credentials(
         username, password, tribe_id, tribe_secret, access_token_url)
 
-    geneset_response = create_remote_geneset(access_token, geneset_info,
+    if create_new_versions:
+        gs_slug = geneset_info['slug']
+
+        complete_gs_slug = username + '/' + gs_slug
+        gs_url = tribe_url + '/api/v1/geneset/' + complete_gs_slug
+
+        parameters = {'oauth_consumer_key': access_token,
+                      'full_annotations': 'true', 'show_tip': 'true',
+                      'xrid': 'Entrez'}
+
+        check_gs_request = requests.get(gs_url, params=parameters)
+
+        if check_gs_request.status_code == 200:
+            gs_response = check_gs_request.json()
+            annotations = gs_response['tip']['annotations']
+
+            old_annotations = {}
+            for annotation in annotations:
+                gene = annotation['gene']['entrezid']
+                old_annotations[gene] = set(annotation['pubs'])
+
+            gene_list = geneset_info['annotations'].keys()
+            gene_entrezids = translate_gene_ids(tribe_url, gene_list,
+                                                geneset_info['xrdb'], 'Entrez')
+
+            pub_set_annotations = {}
+            for gene, publist in geneset_info['annotations'].iteritems():
+                if gene in gene_entrezids:
+                    if len(gene_entrezids[gene]) != 1:
+                        logger.warning('There was more than one Entrez ID '
+                                       'found for gene %s', gene)
+                    gene = gene_entrezids[gene][0]
+                else:
+                    logger.warning('No Entrez IDs were found for gene %s',
+                                   gene)
+
+                pub_set_annotations[gene] = set(publist)
+
+            if pub_set_annotations != old_annotations:
+                geneset_info['geneset'] = gs_response['resource_uri']
+                geneset_info['parent'] = gs_response['tip']['resource_uri']
+                geneset_info['description'] = 'Updating annotations.'
+                response = create_remote_version(access_token, geneset_info,
+                                                 tribe_url)
+            else:
+                # Do not create a new version or geneset
+                logger.info('Geneset with title %s already exists with the '
+                            'same annotations and Tribe. Not attempting to '
+                            'save this geneset.', geneset_info['title'])
+                response = {}
+                response['content'] = (
+                    'There is already a geneset with the slug "{0}" and '
+                    'annotations {1} saved in Tribe. A new geneset has not '
+                    'been saved.').format(geneset_info['slug'],
+                                          geneset_info['annotations'])
+                response['status_code'] = 400
+        else:
+            # No geneset with this geneset 'slug' and creator username exists
+            # yet, so create it
+            response = create_remote_geneset(access_token, geneset_info,
                                              tribe_url)
 
-    return geneset_response
+    else:
+        response = create_remote_geneset(access_token, geneset_info,
+                                         tribe_url)
+
+    return response
 
 
 def return_as_json(geneset_info):
