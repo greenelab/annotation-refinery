9560ede42cf7789c39295b10fcc59068a692b541
rzelayafavila <Rene.Zelaya.F@gmail.com>
2016-05-17 14:00:44 -0400

    Adding docstring for loader.load_to_tribe() function, change response status_code to 409 (instead of 400) if a geneset with the same title and annotations already exists (and change corresponding unit test), and add some defensive logic in case the requested geneset has no 'tip' version in loader.py

--- loader.py
+++ loader.py
@@ -12,6 +12,75 @@ logger.addHandler(logging.NullHandler())
 
 
 def load_to_tribe(main_config_file, geneset_info, create_new_versions=False):
+    """
+    This function takes the passed geneset data (in the geneset_info
+    dictionary) and attempts to create a new geneset or version
+    of a geneset in Tribe using the helper functions in the tribe_client
+    module. It reads the necessary username, password, tribe_id, and
+    tribe_secret from the secrets file defined in the main_config_file
+    and the Tribe location/url from this same main_config_file.
+
+    *Note: This function depends on the tribe-client package, which must be
+    installed in the same python environment as this function is run from.
+
+    Arguments:
+    main_config_file -- A string, location of the main INI configuration
+    file, which should include Tribe parameters and location of secrets
+    file.
+
+    geneset_info -- Dictionary containing the data for the geneset or
+    version to be created. The geneset_info dictionary **must** contain
+    a geneset 'title' and nonempty 'annotations' for a new geneset or
+    geneset version to be created.
+    This is an example of a geneset_info dictionary:
+
+    geneset_info = {
+        'title': 'DO-0014667:disease of metabolism',
+        'abstract':
+            'A disease that involving errors in metabolic processes of '
+            'building or degradation of molecules. Annotations from child'
+            ' terms in the disease ontology are propagated through '
+            'transitive closure. Only annotations with confidence labeled'
+            ' C or P by OMIM have been added.',
+        'xrdb': 'Entrez',
+        'organism': 'Homo sapiens',
+        'annotations': {4160: [], 8431: [], 51738: [], 5443: [],
+                        5468: [], 6492: []},
+
+        'slug': 'doid0014667-homo-sapiens'
+     }
+
+    create_new_versions -- Boolean keyword argument. If False, this function
+    will not try to create new versions of already existing genesets - it will
+    attempt to create a geneset in Tribe from the geneset_info and fail if the
+    geneset (with the same title and creator username) already exists.
+    If True, this function will check if a geneset with the same title and
+    creator username exists. If no such geneset exists, it will create a new
+    geneset, but if such geneset does exist, it will check if the
+    annotations have changed. If the so, it will create a new version of the
+    geneset with the new annotations. If the annotations have not changed, no
+    new geneset version will be saved.
+
+    Returns:
+    Either:
+    a) response -- The response from Tribe, including a 'status_code' and
+    'content'
+
+    or b) False (and raises an error), if the geneset_info did not contain a
+    title or annotations.
+
+    """
+    if 'title' not in geneset_info:
+        logger.error('Data for geneset must contain a "title" key and value'
+                     ' to be able to be saved to Tribe.')
+        return False
+
+    if 'annotations' not in geneset_info or geneset_info['annotations'] == []:
+        logger.error('Data for geneset must contain a "annotations" key and '
+                     'value to be able to be saved to Tribe. The value must '
+                     'be a non-empty list of annotations.')
+        return False
+
     try:
         from tribe_client.utils import obtain_token_using_credentials, \
             create_remote_geneset, create_remote_version
@@ -72,7 +141,16 @@ def load_to_tribe(main_config_file, geneset_info, create_new_versions=False):
 
         if check_gs_request.status_code == 200:
             gs_response = check_gs_request.json()
-            annotations = gs_response['tip']['annotations']
+
+            try:
+                annotations = gs_response['tip']['annotations']
+            except KeyError:
+                # There was no 'tip' version for this geneset, create
+                # first version.
+                geneset_info['geneset'] = gs_response['resource_uri']
+                geneset_info['description'] = 'Adding annotations.'
+                response = create_remote_version(access_token, geneset_info,
+                                                 tribe_url)
 
             old_annotations = {}
             for annotation in annotations:
@@ -97,6 +175,7 @@ def load_to_tribe(main_config_file, geneset_info, create_new_versions=False):
                 pub_set_annotations[gene] = set(publist)
 
             if pub_set_annotations != old_annotations:
+                # Annotations have changed
                 geneset_info['geneset'] = gs_response['resource_uri']
                 geneset_info['parent'] = gs_response['tip']['resource_uri']
                 geneset_info['description'] = 'Updating annotations.'
@@ -114,7 +193,7 @@ def load_to_tribe(main_config_file, geneset_info, create_new_versions=False):
                     'annotations {1} saved in Tribe. A new geneset has not '
                     'been saved.').format(geneset_info['slug'],
                                           geneset_info['annotations'])
-                response['status_code'] = 400
+                response['status_code'] = 409
         else:
             # No geneset with this geneset 'slug' and creator username exists
             # yet, so create it
